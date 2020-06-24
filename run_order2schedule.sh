cd7f0b0e490a55c908939711fa64c4e4ecc2edce
derekmma <hi@derek.ma>
2020-06-23 21:13:16 -0700

    update script

--- run_order2schedule.sh
+++ run_order2schedule.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-input_files="data/demos/order data/papers/order data/srw/order"
+input_files="data/demos/order data/papers/order data/SRW/order"
 output_dir="auto/papers"
 
-python scripts/order2schedule.py -output_dir $output_dir $input_files
+python2 scripts/order2schedule.py -output_dir $output_dir $input_files
