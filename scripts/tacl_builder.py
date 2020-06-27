a6dc2bdd01a90a89472ea0d0f0bc348a22e94340
derekmma <hi@derek.ma>
2020-06-26 19:40:03 -0700

    tacl builder now support CL papers yaml input too

--- scripts/tacl_builder.py
+++ scripts/tacl_builder.py
@@ -17,10 +17,11 @@ from handbook import latex_escape
 import yaml
 
 PARSER = argparse.ArgumentParser(description="Generate schedules for *ACL handbooks")
-PARSER.add_argument("-yaml", default='input/tacl_papers.yaml', type=str, help="File path for TACL YAML file")
+PARSER.add_argument("-yaml_tacl", default='input/tacl_papers.yaml', type=str, help="File path for TACL YAML file")
+PARSER.add_argument("-yaml_cl", default='input/cl_papers.yaml', type=str, help="File path for CL YAML file")
 args = PARSER.parse_args()
 
-for path in ['auto/tacl', 'auto/abstracts']:
+for path in ['auto/tacl', 'auto/cl', 'auto/abstracts']:
     if not os.path.exists(path):
         os.makedirs(path)
 
@@ -31,11 +32,25 @@ def bib(paper):
     str += '  SORTNAME = {%s}}\n\n' % (paper['title'])
     return str
 
-print >> sys.stderr, "Reading from file", args.yaml
+# Process TACL papers
+print >> sys.stderr, "Reading from file", args.yaml_tacl
 print >> sys.stderr, "Write bibtex entries to auto/tacl/papers.bib"
 
 bibfile = codecs.open('auto/tacl/papers.bib', 'w', encoding = 'utf-8')
-for paper in yaml.load(open(args.yaml)):
+for paper in yaml.load(open(args.yaml_tacl)):
+    bibfile.write(bib(paper))
+
+    print >> sys.stderr, "Writing abstract auto/abstracts/%s.tex" % (paper['id'])
+    abstract = codecs.open('auto/abstracts/%s.tex' % (paper['id']), 'w', encoding='utf-8')
+    abstract.write(latex_escape(paper['abstract']))
+    abstract.close()
+
+# Process CL papers
+print >> sys.stderr, "Reading from file", args.yaml_cl
+print >> sys.stderr, "Write bibtex entries to auto/cl/papers.bib"
+
+bibfile = codecs.open('auto/cl/papers.bib', 'w', encoding = 'utf-8')
+for paper in yaml.load(open(args.yaml_cl)):
     bibfile.write(bib(paper))
 
     print >> sys.stderr, "Writing abstract auto/abstracts/%s.tex" % (paper['id'])
