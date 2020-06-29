3ed9c3cdc7d547d9e986c60f6525d88072f82b91
derekmma <hi@derek.ma>
2020-06-28 21:07:43 -0700

    bug fix, show source in TACL CL paper title

--- scripts/tacl_builder.py
+++ scripts/tacl_builder.py
@@ -25,11 +25,11 @@ for path in ['auto/tacl', 'auto/cl', 'auto/abstracts']:
     if not os.path.exists(path):
         os.makedirs(path)
 
-def bib(paper):
+def bib(paper, source_str='[TACL]'):
     str  = '@INPROCEEDINGS{%s,\n' % (paper['id'])
     str += '  AUTHOR = {%s},\n' % (paper['authors'])
-    str += '  TITLE = {%s},\n' % (latex_escape(paper['title']))
-    str += '  SORTNAME = {%s}}\n\n' % (paper['title'])
+    str += '  TITLE = {%s %s},\n' % (source_str, latex_escape(paper['title_str']))
+    str += '  SORTNAME = {%s}}\n\n' % (paper['title_str'])
     return str
 
 # Process TACL papers
@@ -38,7 +38,7 @@ print >> sys.stderr, "Write bibtex entries to auto/tacl/papers.bib"
 
 bibfile = codecs.open('auto/tacl/papers.bib', 'w', encoding = 'utf-8')
 for paper in yaml.load(open(args.yaml_tacl)):
-    bibfile.write(bib(paper))
+    bibfile.write(bib(paper, source_str='[TACL]'))
 
     print >> sys.stderr, "Writing abstract auto/abstracts/%s.tex" % (paper['id'])
     abstract = codecs.open('auto/abstracts/%s.tex' % (paper['id']), 'w', encoding='utf-8')
@@ -51,7 +51,7 @@ print >> sys.stderr, "Write bibtex entries to auto/cl/papers.bib"
 
 bibfile = codecs.open('auto/cl/papers.bib', 'w', encoding = 'utf-8')
 for paper in yaml.load(open(args.yaml_cl)):
-    bibfile.write(bib(paper))
+    bibfile.write(bib(paper, source_str='[CL]'))
 
     print >> sys.stderr, "Writing abstract auto/abstracts/%s.tex" % (paper['id'])
     abstract = codecs.open('auto/abstracts/%s.tex' % (paper['id']), 'w', encoding='utf-8')
