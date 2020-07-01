83cb16eb9d742bdfaf96d224ebb5a397f0104644
derekmma <hi@derek.ma>
2020-07-01 00:40:33 -0700

    typo fix

--- run_order2schedule.sh
+++ run_order2schedule.sh
@@ -2,6 +2,6 @@
 
 # input_files="data/papers/order data/SRW/order data/demos/order"
 input_files="data/papers/order"
-output_dir="auto/papers"
+output_dir="auto/schedule"
 
 python2 scripts/order2schedule.py -output_dir $output_dir $input_files
